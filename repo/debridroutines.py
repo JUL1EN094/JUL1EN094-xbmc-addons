4615298c6d58f8bda5a1e608ec6002ed5c128582
JUL1EN094 <jujul1en094@gmail.com>
2013-11-05 02:05:38 +0100

    Update debridroutine.py

--- repo/debridroutines.py
+++ repo/debridroutines.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-   
+#---------------------------------------------------------------------
+"""
+debridroutine python library
+Copyright (C) 2013 JUL1EN094
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
 import urllib, urllib2
 import re, os, cookielib
 
@@ -17,7 +36,7 @@ class RealDebrid:
             cj = cookielib.LWPCookieJar()
             cj.load(self.cookie_file)
             req = urllib2.Request(url)
-            req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')   
+            req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
             opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
             response = opener.open(req)
 
@@ -31,8 +50,8 @@ class RealDebrid:
                 return link
 
             #if we have been redirected, return the redirect url
-            elif finalurl is not url:               
-                return finalurl    
+            elif finalurl is not url:
+                return finalurl
 
 
     def Resolve(self, url, quality):
@@ -46,15 +65,20 @@ class RealDebrid:
 
 
     def valid_host(self, host):
-        url = 'http://real-debrid.com/lib/api/hosters.php'
-        allhosts = self.GetURL(url)
-        if host in allhosts:
-            return True
-        else:
+        try :
+            url = 'http://www.real-debrid.com/api/regex.php?type=all'
+            response = self.GetURL(url).lstrip('/').rstrip('/g')
+            delim = '/g,/|/g\|-\|/'
+            allhosts = [re.compile(host) for host in re.split(delim, response)]
+            if host in allhosts:
+                return True
+            else:
+                return False            
+        except:
             return False
 
 
-    def  checkLogin(self):
+    def checkLogin(self):
         url = 'https://real-debrid.com/api/account.php'
         source = self.GetURL(url)
         if source is not None and re.search('expiration', source):
@@ -63,7 +87,7 @@ class RealDebrid:
             return True
 
 
-    def Login(self):    
+    def Login(self):
         if self.checkLogin():
             cj = cookielib.LWPCookieJar()
             login_data = urllib.urlencode({'user' : self.username, 'pass' : self.password})
@@ -85,3 +109,4 @@ class RealDebrid:
                 return False
         else:
             return True
+ 
