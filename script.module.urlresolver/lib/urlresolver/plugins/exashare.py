219f733d202d138f724f1ef32133478ebb9ed40a
JUL1EN094 <jujul1en094@gmail.com>
2014-11-13 10:41:58 +0100

    Up urlresolver -> v 2.5.0.003

--- script.module.urlresolver/lib/urlresolver/plugins/exashare.py
+++ script.module.urlresolver/lib/urlresolver/plugins/exashare.py
@@ -21,6 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import urllib, urllib2, os, re
 from t0mm0.common.net import Net
 from urlresolver.plugnplay.interfaces import UrlResolver
+from urlresolver.plugnplay.interfaces import SiteAuth
 from urlresolver.plugnplay.interfaces import PluginSettings
 from urlresolver.plugnplay import Plugin
 from urlresolver import common
@@ -31,19 +32,29 @@ error_logo = os.path.join(common.addon_path, 'resources', 'images', 'redx.png')
 ok_logo = os.path.join(common.addon_path, 'resources', 'images', 'greeninch.png')
 
 class ExashareResolver(Plugin, UrlResolver, PluginSettings):
-    implements = [UrlResolver, PluginSettings]
-    name = "exashare"    
+    implements = [UrlResolver, SiteAuth, PluginSettings]
+    name = "exashare"
+    profile_path = common.profile_path    
+    cookie_file  = os.path.join(profile_path, '%s.cookies' % name)    
 
     def __init__(self):
         p = self.get_setting('priority') or 100
         self.priority = int(p)
         self.net = Net()
 
+    #UrlResolver methods
     def get_media_url(self, host, media_id):
         base_url = 'http://www.'+host+'.com/'+media_id
         try:
             html  = self.net.http_GET(base_url).content
             stream_url = re.findall('file: "([^"]+)"', html)[0]
+            if self.get_setting('login') == 'true' :  
+                cookies = {}
+                for cookie in self.net._cj:
+                    cookies[cookie.name] = cookie.value
+                if len(cookies)>0 : 
+                    stream_url = stream_url + '|' + urllib.urlencode({'Cookie' :urllib.urlencode(cookies)}) 
+            common.addon.log('stream_url : '+stream_url)
             return stream_url
         except urllib2.HTTPError, e:
             e = e.code
@@ -87,6 +98,48 @@ class ExashareResolver(Plugin, UrlResolver, PluginSettings):
             return False
         return re.match('http://(www.)?exashare.com/(embed\-)?[0-9A-Za-z]+(\-[0-9]+x[0-9]+.html)?',url) or 'exashare.com' in host    
 
+    #SiteAuth methods
+    def needLogin(self):
+        url = 'http://www.exashare.com/?op=my_account'
+        if not os.path.exists(self.cookie_file):
+            common.addon.log_debug('needLogin returning True')
+            return True
+        self.net.set_cookies(self.cookie_file)
+        source = self.net.http_GET(url).content
+        if re.search("""Your username is for logging in and cannot be changed""", source) :
+            common.addon.log_debug('needLogin returning False')
+            return False
+        else :
+            common.addon.log_debug('needLogin returning True')
+            return True
+    
+    def login(self):
+        if (self.get_setting('login')=='true') and self.needLogin() :
+            common.addon.log('logging in exashare')
+            url = 'http://www.exashare.com/'
+            data = {'login':self.get_setting('username') , 'password':self.get_setting('password') , 'op':'login' , 'redirect':'/login.html'}        
+            source = self.net.http_POST(url,data).content
+            if re.search('Your username is for logging in and cannot be changed', source):            
+                common.addon.log('logged in exashare')
+                common.addon.show_small_popup(title='[B][COLOR white]EXASHARE LOGIN [/COLOR][/B]', msg='[COLOR green]Logged[/COLOR]', delay=2000, image=ok_logo)
+                self.net.save_cookies(self.cookie_file)
+                self.net.set_cookies(self.cookie_file)
+                return True
+            else:
+                common.addon.log('error logging in exashare')
+                common.addon.show_small_popup(title='[B][COLOR white]EXASHARE LOGIN ERROR [/COLOR][/B]', msg='[COLOR red]Not logged[/COLOR]', delay=2000, image=error_logo)
+                return False
+        else :
+            common.addon.show_small_popup(title='[B][COLOR white]EXASHARE LOGIN [/COLOR][/B]', msg='[COLOR green]Logged[/COLOR]', delay=2000, image=ok_logo)
+            return True
+                    
+    #PluginSettings methods
     def get_settings_xml(self):
         xml = PluginSettings.get_settings_xml(self)
+        xml += '<setting id="ExashareResolver_login" '        
+        xml += 'type="bool" label="Login" default="false"/>\n'
+        xml += '<setting id="ExashareResolver_username" enable="eq(-1,true)" '
+        xml += 'type="text" label="     username" default=""/>\n'
+        xml += '<setting id="ExashareResolver_password" enable="eq(-2,true)" '
+        xml += 'type="text" label="     password" option="hidden" default=""/>\n'
         return xml            
